9c9f537004a2856d8dcf75b79438e848e87f082a
Wiktor Wichrowski <wiktorw39@gmail.com>
2021-06-30 21:57:53 +0200

    dodanie operacji na liczbach, i bitowych

--- computer.h
+++ computer.h
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
 
-#define COUNT_OF(x) \
-  ((sizeof(x) / sizeof(0 [x])) / ((size_t)(!(sizeof(x) % sizeof(0 [x])))))
 
 template <class Instruction, class... Rest>
 class Program;
